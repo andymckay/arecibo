16a4a97980206fe87b01e987278c23826247f2f5
Chris Adams <chris@improbable.org>
2010-06-09 10:57:59 -0400

    Receiver: set correct MIME type for POST response

--- listener/app_engine/receiving/http.py
+++ listener/app_engine/receiving/http.py
@@ -4,10 +4,10 @@ from error.models import Error
 from receiving.post import populate
 
 def post(request):
-    """ Add in a post """ 
+    """ Add in a post """
     err = Error()
     err.ip = request.META.get("REMOTE_ADDR", "")
     err.user_agent = request.META.get("HTTP_USER_AGENT", "")
 
     populate(err, request.POST)
-    return HttpResponse("Error recorded")
+    return HttpResponse("Error recorded",  mimetype="text/plain")
