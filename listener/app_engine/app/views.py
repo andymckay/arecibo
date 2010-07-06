6dcb651ca7106753c26f82013b52abbedbd17feb
Chris Adams <chris@improbable.org>
2010-07-06 15:07:50 -0400

    JavaScript Client: far-future caching
    This avoids having the browser make a separate request for our
    JavaScript library on every request but does mean that significant
    future client updates would require changing the URL in some way -
    either us changing the URLs to be something like /js/<VERSION>/error.js
    (arecibo.js?) or simply being careful not to break backwards
    compatibility on future updates.

--- listener/app_engine/app/views.py
+++ listener/app_engine/app/views.py
@@ -6,8 +6,14 @@ from django.http import HttpResponseRedirect
 from django.views.generic.simple import direct_to_template
 from django.core.urlresolvers import reverse
 from django.conf import settings
+from django.views.decorators.vary import vary_on_headers
+from django.views.decorators.cache import cache_control
 
 from google.appengine.api import users
+
+#: static_resource cache decorator will be used for things which can safely
+# be cached to improve client HTTP performance
+static_resource = cache_control(public=True, max_age=86400)
 
 def index(request):
     if request.user.is_authenticated and request.user.is_staff:
@@ -21,6 +27,8 @@ def setup(request):
         "app_id": os.environ.get("APPLICATION_ID"),
         })
 
+@static_resource
+@vary_on_headers("Accept-Encoding")
 def javascript_client(request):
     return direct_to_template(request, "error.js",
         extra_context = {
