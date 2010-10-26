7f3f7dd883210d2ec19a143ac15cc968980abde2
Andy McKay <amckay@mozilla.com>
2010-10-26 11:50:15 -0700

    deactive on process exception

--- listener/app_engine/userstorage/middleware.py
+++ listener/app_engine/userstorage/middleware.py
@@ -4,6 +4,9 @@ class UserStorage:
     def process_request(self, request):
         activate(request)
 
+    def process_exception(self, request, exception):
+        deactivate(request)
+
     def process_response(self, request, response):
         deactivate(request)
         return response
